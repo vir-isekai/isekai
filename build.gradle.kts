bf13d835eecbf7fa980bd7a9b37716c8db1fb460
ywonp94 <ywonp94@gmail.com>
2025-02-26 18:11:26 +0900

    :necktie: allOpen SubModule에 부여 및 BaseTimeEntity 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,18 +46,18 @@ subprojects {
 		testImplementation("io.mockk:mockk:1.13.9")
 		implementation("com.appmattus.fixture:fixture:1.1.0")
 	}
+
+	allOpen {
+		annotation("jakarta.persistence.Entity")
+		annotation("jakarta.persistence.MappedSuperclass")
+		annotation("jakarta.persistence.Embeddable")
+	}
 }
 
 kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
-}
-
-allOpen {
-	annotation("jakarta.persistence.Entity")
-	annotation("jakarta.persistence.MappedSuperclass")
-	annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
