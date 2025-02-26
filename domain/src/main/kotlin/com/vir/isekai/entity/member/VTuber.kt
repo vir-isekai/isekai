58a661efa34796ceab1e916605d2c1474020673d
yangwon-park <ywonp94@gmail.com>
2025-02-26 22:59:25 +0900

    :necktie: VTuber Entity 2차 구현

--- domain/src/main/kotlin/com/vir/isekai/entity/member/VTuber.kt
+++ domain/src/main/kotlin/com/vir/isekai/entity/member/VTuber.kt
@@ -1,6 +1,8 @@
 package com.vir.isekai.entity.member
 
 import com.vir.isekai.entity.Agency
+import com.vir.isekai.entity.BaseTimeEntity
+import com.vir.isekai.entity.enums.RaceType
 import jakarta.persistence.*
 
 @Entity
@@ -11,9 +13,18 @@ class VTuber(
 	@Column(name = "v_tuber_id")
 	val id: Long? = null,
 
-	@OneToOne(cascade = [CascadeType.DETACH])
+	@OneToOne(cascade = [CascadeType.REMOVE])
 	@JoinColumn(name = "agency_id")
 	val agency: Agency,
 
 	val name: String,
-)
+
+	val age: Int,
+
+	val height: Int,
+
+	val fandomName: String? = null,
+
+	@Enumerated(EnumType.STRING)
+	val race: RaceType,
+) : BaseTimeEntity()
