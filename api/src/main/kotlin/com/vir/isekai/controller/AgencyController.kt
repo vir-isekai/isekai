8347d06165b9ce3f9a6edd4fb3a2cbd39f496ba4
yangwon-park <ywonp94@gmail.com>
2025-02-27 12:52:04 +0900

    :necktie: AgencyFacade AgencyService Layer 구현

--- api/src/main/kotlin/com/vir/isekai/controller/AgencyController.kt
+++ api/src/main/kotlin/com/vir/isekai/controller/AgencyController.kt
@@ -1,13 +1,16 @@
 package com.vir.isekai.controller
 
 import com.vir.isekai.dto.CommonResponse
+import com.vir.isekai.facade.AgencyFacade
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/agencies")
-class AgencyController() {
+class AgencyController(
+	private val agencyFacade: AgencyFacade,
+) {
 	@PostMapping
 	fun saveAgency(): CommonResponse<Nothing> {
 		return CommonResponse.successSave()
---
+++ api/src/main/kotlin/com/vir/isekai/facade/AgencyFacade.kt
@@ -0,0 +1,6 @@
+package com.vir.isekai.facade
+
+import org.springframework.stereotype.Service
+
+@Service
+class AgencyFacade
