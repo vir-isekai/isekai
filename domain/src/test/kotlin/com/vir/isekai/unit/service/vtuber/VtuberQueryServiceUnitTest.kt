da10d34cf5dd89b05b7830ed17408f57eca828fe
ywonp94 <ywonp94@gmail.com>
2025-03-07 11:23:32 +0900

    :white_check_mark: Test 코드 에러 해결

--- domain/src/test/kotlin/com/vir/isekai/unit/service/vtuber/VtuberQueryServiceUnitTest.kt
+++ domain/src/test/kotlin/com/vir/isekai/unit/service/vtuber/VtuberQueryServiceUnitTest.kt
@@ -1,10 +1,11 @@
 package com.vir.isekai.unit.service.vtuber
 
 import com.vir.isekai.dto.command.VtuberCommand
+import com.vir.isekai.entity.Channel
 import com.vir.isekai.entity.Vtuber
 import com.vir.isekai.entity.enums.Generation
-import com.vir.isekai.entity.enums.Platform
 import com.vir.isekai.repository.agency.AgencyRepository
+import com.vir.isekai.repository.channel.ChannelRepository
 import com.vir.isekai.repository.vtuber.VtuberRepository
 import com.vir.isekai.service.vtuber.VtuberQueryService
 import io.kotest.core.spec.style.StringSpec
@@ -14,12 +15,14 @@ import org.springframework.data.repository.findByIdOrNull
 class VtuberQueryServiceUnitTest : StringSpec({
 	val agencyRepository: AgencyRepository = mockk()
 	val vtuberRepository: VtuberRepository = mockk()
+	val channelRepository: ChannelRepository = mockk()
 
 	lateinit var service: VtuberQueryService
 
 	beforeTest {
-		clearMocks(agencyRepository, vtuberRepository)
-		service = VtuberQueryService(agencyRepository, vtuberRepository)
+		clearMocks(agencyRepository, channelRepository, vtuberRepository)
+
+		service = VtuberQueryService(agencyRepository, channelRepository, vtuberRepository)
 	}
 
 	val command =
@@ -31,7 +34,7 @@ class VtuberQueryServiceUnitTest : StringSpec({
 			160,
 			Generation.NONE,
 			"인간",
-			Platform.CHZZK,
+			mutableListOf(),
 		)
 
 	val commandWithAgency =
@@ -43,24 +46,36 @@ class VtuberQueryServiceUnitTest : StringSpec({
 			160,
 			Generation.FIRST,
 			"인간",
-			Platform.CHZZK,
+			mutableListOf(),
 		)
 
 	"소속 없는 버튜버 저장 성공" {
+		val mockChannel1 = mockk<Channel>(relaxed = true)
+		val mockChannel2 = mockk<Channel>(relaxed = true)
+		val mockChannels = listOf(mockChannel1, mockChannel2) // 리스트 자체는 실제 List 사용
+
 		every { vtuberRepository.save(any(Vtuber::class)) } returns mockk()
+		every { channelRepository.saveAll(any<List<Channel>>()) } returns mockChannels
 
 		service.saveVtuber(command)
 
 		verify(exactly = 1) { vtuberRepository.save(any(Vtuber::class)) }
+		verify(exactly = 1) { channelRepository.saveAll(any<List<Channel>>()) }
 	}
 
 	"소속 있는 버튜버 저장 성공" {
+		val mockChannel1 = mockk<Channel>(relaxed = true)
+		val mockChannel2 = mockk<Channel>(relaxed = true)
+		val mockChannels = listOf(mockChannel1, mockChannel2) // 리스트 자체는 실제 List 사용
+
 		every { agencyRepository.findByIdOrNull(commandWithAgency.agencyId) } returns mockk()
 		every { vtuberRepository.save(any(Vtuber::class)) } returns mockk()
+		every { channelRepository.saveAll(any<List<Channel>>()) } returns mockChannels
 
 		service.saveVtuber(commandWithAgency)
 
 		verify(exactly = 1) { agencyRepository.findByIdOrNull(commandWithAgency.agencyId) }
 		verify(exactly = 1) { vtuberRepository.save(any(Vtuber::class)) }
+		verify(exactly = 1) { channelRepository.saveAll(any<List<Channel>>()) }
 	}
 })
