906ce62f98910c3658091821bd4e1dd512b45be7
ywonp94 <ywonp94@gmail.com>
2025-03-26 18:42:28 +0900

    :heavy_plus_sign: Spring Security, JWT 관련 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,6 +6,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
+
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.security:spring-security-test")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.named("bootJar") {
