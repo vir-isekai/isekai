059473b9dc73274bb492eb07b8aedc953cf2104e
yangwon-park <ywonp94@gmail.com>
2025-05-20 11:34:51 +0900

    :construction: 단일 모륟 변경 후 DTO 부분 리팩토링 중

--- src/main/kotlin/com/vir/isekai/dto/request/FandomRequest.kt
+++ src/main/kotlin/com/vir/isekai/dto/request/FandomRequest.kt
@@ -1,5 +1,9 @@
 package com.vir.isekai.dto.request
 
+import com.vir.isekai.entity.Agency
+import com.vir.isekai.entity.Fandom
+import com.vir.isekai.entity.Vtuber
+
 class FandomRequest {
 	data class Save(
 		val agencyId: Long?,
@@ -7,10 +11,34 @@ class FandomRequest {
 		val name: String,
 		val logoImageUrl: String,
 	) {
-		fun toEntityWithAgency() {
+		fun toEntityWithAgency(agency: Agency): Fandom {
+			return Fandom(
+				null,
+				agency,
+				null,
+				name,
+				logoImageUrl,
+			)
 		}
 
-		fun toEntityWithVtuber() {
+		fun toEntityWithVtuber(vtuber: Vtuber): Fandom {
+			return Fandom(
+				null,
+				null,
+				vtuber,
+				name,
+				logoImageUrl,
+			)
+		}
+
+		fun toEntity(): Fandom {
+			return Fandom(
+				null,
+				null,
+				null,
+				name,
+				logoImageUrl,
+			)
 		}
 	}
 }
